0ae5afecb4443561eb322235768bac13802c1073
Victor <victoraweb@gmail.com>
2017-07-25 21:19:45 -0300

    [FEAT] Worker processing one task at a time

--- consumer.js
+++ consumer.js
@@ -14,14 +14,17 @@ amqp.connect('amqp://localhost')
     // Also creating the queue here because the consumer might start before the publisher so we want to make
     // sure it already exists.
     ch.assertQueue(q.Q_IN_REQ, { durable: true });
+    // Get one message at a time
+    ch.prefetch(1);
 
     ch.consume(q.Q_IN_REQ, msg => {
         const secs = msg.content.toString().split('.').length -1;
         console.log(`Received message [${ new Date().getTime() }]: ${ msg.content.toString() }`)
         setTimeout(() => {
             console.log('Request processed!');
+            ch.ack(msg);
         }, secs * 1000);
-    }, { noAck: true });
+    }, { noAck: false });
 })
 .catch(err => {
     console.error(err);
